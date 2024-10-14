9a63098a8f279ddd5a0c5e59de2de18935c05252
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-10-14 14:32:42 +0300

    Обновленные download_shopping_cart, модели recipes, api.views, short_link

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,4 @@
-﻿import shortuuid
-from django.conf import settings
+﻿from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
