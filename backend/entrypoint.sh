7640d5cc45e05006e80334069ed7f04e388e759e
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-10-18 08:49:55 +0300

    About/Technologies works, fixing static

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,6 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input --clear
 python manage.py first_time_setup
 
-cp -r /app/collected_static/. /backend_static/static/
+cp -r /app/collected_static/. /backend_static/
 
 gunicorn --bind 0.0.0.0:8080 foodgram_backend.wsgi
