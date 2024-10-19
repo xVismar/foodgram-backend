e72bc9767d7bcf96cca560f5f74ee57d42b86c36
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-10-19 21:14:19 +0300

    README Update, redirect_short_link logic update

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-from api.views import redirect_short_link
+from api.views import redirect_short_link, RecipeViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,11 +12,16 @@ urlpatterns = [
     path(
         'about/',
         RedirectView.as_view(url='/static/pages/about/index.html'),
-        name='about'
+        name='О проекте'
     ),
     path(
         'technologies/',
         RedirectView.as_view(url='/static/pages/technologies/index.html'),
-        name='technologies'
+        name='Технологии'
     ),
+    path(
+        'recipes/<int:pk>/',
+        RecipeViewSet.as_view({'get': 'retrieve'}),
+        name='recipe-detail'
+    )
 ]
