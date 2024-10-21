ec79209423ac33b35df94f2dcd65306a0977b29c
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-10-22 02:40:05 +0300

    Обновления после четвертого ревью

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,7 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input --clear
 cp -r /app/collected_static/. /backend_static/static
-python manage.py first_time_setup
+python manage.py import_ingredients
+python manage.py import_tags
 
 gunicorn --bind 0.0.0.0:8080 foodgram_backend.wsgi
