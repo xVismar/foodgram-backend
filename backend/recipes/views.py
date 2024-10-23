ab422c4ad9ae356991a64db882238f4f14cd0a92
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-10-23 21:47:53 +0300

    small fixes, review ready

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
+from django.conf import settings
 
 
 def redirect_short_link(request, short_id):
-    return redirect(f'recipes/{short_id}')
+    return redirect(settings.HOME_DOMAIN + f'recipes/{short_id}')
